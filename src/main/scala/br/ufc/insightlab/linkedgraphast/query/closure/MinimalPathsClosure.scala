6c412a64ce93706d7af8e6849ad54fac9fac19d0
Lucas Peres <lucaspg96@live.com>
2019-05-08 11:24:21 -0300

    [#4] Implementing first version of Minimal Paths Closure

--- src/main/scala/br/ufc/insightlab/linkedgraphast/query/closure/MinimalPathsClosure.scala
+++ src/main/scala/br/ufc/insightlab/linkedgraphast/query/closure/MinimalPathsClosure.scala
@@ -1,9 +1,11 @@
 package br.ufc.insightlab.linkedgraphast.query.closure
 
 import br.ufc.insightlab.graphast.model.{Edge, Node}
+import br.ufc.insightlab.graphast.query.shortestpath.DijkstraStrategy
 import br.ufc.insightlab.linkedgraphast.model.graph.LinkedGraph
-import br.ufc.insightlab.linkedgraphast.model.link.{Link, Relation}
+import br.ufc.insightlab.linkedgraphast.model.link.{Attribute, Link, Relation}
 import br.ufc.insightlab.linkedgraphast.model.node.URI
+import br.ufc.insightlab.linkedgraphast.parser.NTripleParser
 import br.ufc.insightlab.linkedgraphast.query.MinimalPaths.MinimalFinder.MinimalPathsFinder
 import br.ufc.insightlab.linkedgraphast.query.MinimalPaths.utils.{Path, PathMultipleEdge, PathSingleEdge}
 import org.slf4j.{Logger, LoggerFactory}
@@ -19,18 +21,7 @@ object MinimalPathsClosure extends Closure {
 
     if (nodes.size == 1) graph
     else {
-      g.getLinksAsStream
-        .foreach(link => link match {
-          case Relation(_, l, t) =>
-            if ((l.uri.endsWith("#type") && (t.uri.endsWith("#Class") || t.uri.endsWith("Property"))) ||
-              (l.uri.endsWith("#range") && t.uri.contains("XMLSchema#")))
-              link.setWeight(100)
-            else if (l.uri.contains("rdf-schema#sub"))
-              link.setWeight(10)
-
-          case _ =>
-        })
-
+      ClosureHelper.adjustEdgesWeight(g)
 
       val pairNodes = nodes
         .combinations(2)
@@ -40,12 +31,34 @@ object MinimalPathsClosure extends Closure {
 
       var modifiedEdges = List[(Edge, Double)]()
 
+      var varPropertiesCount = 0
       for ((n1, n2) <- pairNodes) {
-        println(s"Computing path from ${g.getNode(n1)} to ${g.getNode(n2)}")
-        val paths = MinimalPathsFinder(g, n1, n2)
+//        println(s"Computing path from ${g.getNode(n1)} to ${g.getNode(n2)}")
 
-        for (path <- paths) {
-          println(path)
+        val paths = MinimalPathsFinder(g, n1, n2)
+//        if(paths.nonEmpty) {
+//          println(s"${paths.size} paths found with cost: ${paths.head.cost}")
+//        }
+
+        val compressedPaths = (for (path <- paths) yield {
+          val compressedPath = compressPath(path, varPropertiesCount)
+//          println(s"Path: $path\nCompressed Path: $compressedPath\n--------------------")
+          compressedPath
+        }).distinct
+
+        varPropertiesCount += compressedPaths.count(_.exists(_.source.uri.startsWith("?")))
+
+//        println(compressedPaths.mkString("\n"))
+        for{
+          path <- compressedPaths
+          link <- path
+        }{
+          if(!graph.containsNode(link.source.getId))
+            graph.addNode(link.source)
+          if(!graph.containsNode(link.target.getId))
+            graph.addNode(link.target)
+
+          graph.addLink(link)
         }
 
       }
@@ -66,12 +79,80 @@ object MinimalPathsClosure extends Closure {
 
     }
 
-//  private def compressPath(path: Path): List[Link] = {
-//    for (pair <- path.edges.sliding(2, 1)) {
-//      pair match {
-//        case PathMultipleEdge(edges1) :: PathMultipleEdge(edges2) :: Nil =>
-//        case l1 :: Nil =>
-//      }
-//    }
-//  }
+  private def compressPath(path: Path, c: Int = 0): List[Link] = {
+    var i = c
+    var hasProcessedProperty = false
+    (for (pair <- path.edges.sliding(2, 1)) yield {
+      pair match {
+        case PathMultipleEdge(edges1) :: PathMultipleEdge(edges2) :: Nil =>
+          val l1 = edges1.head.asInstanceOf[Link]
+          val l2 = edges2.head.asInstanceOf[Link]
+          if(isPropertyDefinition(l1, l2)){
+            hasProcessedProperty = true
+            i += 1
+            List(Relation(URI(s"?p${i}"),l1.uri, l1.target.asInstanceOf[URI]), Relation(URI(s"?p${i}"),l2.uri, l2.target.asInstanceOf[URI]))
+          } else if(hasProcessedProperty){
+            hasProcessedProperty = false
+            Nil
+          }  else List(l1)
+
+
+        case PathSingleEdge(edge1) :: PathMultipleEdge(edges2) :: Nil =>
+          val l1 = edge1.asInstanceOf[Link]
+          val l2 = edges2.head.asInstanceOf[Link]
+          if(isPropertyDefinition(l1, l2)){
+            hasProcessedProperty = true
+            i += 1
+            List(Relation(URI(s"?p${i}"),l1.uri, l1.target.asInstanceOf[URI]), Relation(URI(s"?p${i}"),l2.uri, l2.target.asInstanceOf[URI]))
+          } else if(hasProcessedProperty){
+            hasProcessedProperty = false
+            Nil
+          }  else List(l1)
+
+        case PathMultipleEdge(edges1) :: PathSingleEdge(edge2) :: Nil =>
+          val l1 = edges1.head.asInstanceOf[Link]
+          val l2 = edge2.asInstanceOf[Link]
+          if(isPropertyDefinition(l1, l2)){
+            hasProcessedProperty = true
+            i += 1
+            List(Relation(URI(s"?p${i}"),l1.uri, l1.target.asInstanceOf[URI]), Relation(URI(s"?p${i}"),l2.uri, l2.target.asInstanceOf[URI]))
+          } else if(hasProcessedProperty){
+            hasProcessedProperty = false
+            Nil
+          }  else List(l1)
+
+        case PathSingleEdge(edge1) :: PathSingleEdge(edge2) :: Nil =>
+          val l1 = edge1.asInstanceOf[Link]
+          val l2 = edge2.asInstanceOf[Link]
+          if(isPropertyDefinition(l1, l2)){
+            hasProcessedProperty = true
+            i += 1
+            List(Relation(URI(s"?p${i}"),l1.uri, l1.target.asInstanceOf[URI]), Relation(URI(s"?p${i}"),l2.uri, l2.target.asInstanceOf[URI]))
+          } else if(hasProcessedProperty){
+            hasProcessedProperty = false
+            Nil
+          }  else List(l1)
+
+        case PathSingleEdge(edge) :: Nil =>
+          if(hasProcessedProperty) Nil else List(edge.asInstanceOf[Link])
+        case PathMultipleEdge(edges) :: Nil =>
+          if(hasProcessedProperty) Nil else List(edges.head.asInstanceOf[Link])
+      }
+    }).toList.flatten
+  }
+
+  def main(args: Array[String]): Unit = {
+    val graph = NTripleParser.parse("src/main/resources/dbpedia.nt")
+
+    val fromNode = graph.getNodeByURI("person@en")
+    val toNode = graph.getNodeByURI("award@en")
+
+    val nodes = List(fromNode, toNode)
+
+    val fragment = MinimalPathsClosure(graph)(nodes)
+
+    println(fragment.linksAsString())
+
+
+  }
 }
