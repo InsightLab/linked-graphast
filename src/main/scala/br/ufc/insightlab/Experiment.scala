e3675272d245f15761fe5a0f1485ff4eac6b3671
Lucas Peres <lucaspg96@live.com>
2019-06-07 15:53:25 -0300

    Getting highest degrees nodes

--- src/main/scala/br/ufc/insightlab/Experiment.scala
+++ src/main/scala/br/ufc/insightlab/Experiment.scala
@@ -3,6 +3,7 @@ package br.ufc.insightlab
 import br.ufc.insightlab.linkedgraphast.modules.NER.wikifier.Wikifier
 import br.ufc.insightlab.linkedgraphast.modules.vonqbe.{VonQBEFragmentExtractor, VonQBESparqlBuilder}
 import br.ufc.insightlab.linkedgraphast.parser.NTripleParser
+import scala.collection.JavaConverters._
 
 object Experiment extends App {
 
@@ -16,6 +17,13 @@ object Experiment extends App {
   val useNer = true
   val ner = Wikifier
 
+
+  val sortedNodes = graph
+    .getNodes.asScala
+    .map(n => (n,graph.getInEdges(n.getId).asScala.size))
+    .toStream.sortBy(-_._2)
+
+  println(sortedNodes.take(10).mkString("\n"))
 
   for(s <- searches){
     val query2 = new VonQBESparqlBuilder(graph, ner).generateSPARQL(s, useNer)
